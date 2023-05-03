7f431a49e15bdd655f351fe1ae514d7e1a5da40d
APPLE MAC HUB <applemachub@APPLEs-MacBook-Pro.local>
2023-05-04 01:32:59 +0500

    added api key

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from starlette.responses import RedirectResponse
 
 from leadgen_model import do_work
 
+
+valid_api_keys = ["8b60e192049fa9448fb57094"]
+
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
@@ -17,9 +20,15 @@ async def redirect():
 
 
 @app.post("/leadtoopp", response_class=HTMLResponse)
-def read_root(request: Request, file1: UploadFile = File(...), file2: UploadFile = File(...), model_type: str = Form(...)):
-    prediction_results = do_work(file1.file, file2.file, model_type)
+def read_root(request: Request, file1: UploadFile = File(...), file2: UploadFile = File(...), model_type: str = Form(...), api_key: str=Form(...)):
     accept = request.headers.get("Accept")
+    if api_key not in valid_api_keys:
+        if accept and "text/html" in accept:
+            return templates.TemplateResponse("index.html", {"request": request, "error_message": "Invalid API Key", "error": True})
+        else:
+            return JSONResponse(content={"status_code": 401, "message": "Invalid API key"})
+    prediction_results = do_work(file1.file, file2.file, model_type)
+    
     if accept and "text/html" in accept:
         return templates.TemplateResponse("index.html", {"request": request, "message": "Request processed successfully", "data": prediction_results.to_html()})
     else:
