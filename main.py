3aec317460cd4a72b8ccd8217436801885837f78
APPLE MAC HUB <applemachub@APPLEs-MacBook-Pro.local>
2023-05-04 00:24:50 +0500

    added rest api response

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import uvicorn
 from fastapi import FastAPI, Request, UploadFile, File, Form
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.templating import Jinja2Templates
 from starlette.responses import RedirectResponse
 
@@ -19,7 +19,12 @@ async def redirect():
 @app.post("/leadtoopp", response_class=HTMLResponse)
 def read_root(request: Request, file1: UploadFile = File(...), file2: UploadFile = File(...), model_type: str = Form(...)):
     prediction_results = do_work(file1.file, file2.file, model_type)
-    return templates.TemplateResponse("index.html", {"request": request, "message": "Request processed successfully", "data": prediction_results.to_html()})
+    accept = request.headers.get("Accept")
+    if accept and "text/html" in accept:
+        return templates.TemplateResponse("index.html", {"request": request, "message": "Request processed successfully", "data": prediction_results.to_html()})
+    else:
+        new_prediction_results = prediction_results.to_dict(orient="records")
+        return JSONResponse(content={"message": "Request processed successfully", "data": new_prediction_results})
 
 
 @app.get("/leadtoopp", response_class=HTMLResponse)
