a4731094aa52ab76a3d33282cdb1dde7eca542f2
APPLE MAC HUB <applemachub@APPLEs-MacBook-Pro.local>
2023-05-05 02:04:39 +0500

    added api key file logic

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 from oppornot import oppornot
 from regenerate_model import regenerate_model
 import uvicorn
@@ -6,6 +7,7 @@ from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.templating import Jinja2Templates
 from starlette.responses import RedirectResponse
 import secrets
+import os
 from leadgen_model import do_work
 
 
@@ -28,17 +30,39 @@ def generate_api_key(request: Request,  api_key: str=Form(...)):
         return JSONResponse(content={"status_code": 401, "message": "Invalid API key"})
     random_bytes = secrets.token_bytes(32)
     api_key = random_bytes.hex()
+    if os.path.isfile("api_keys.txt"):
+        with open("api_keys.txt", "r") as f:
+            json_string = f.read()
+        api_key_list = json.loads(json_string)
+        api_key_list.append(api_key)
+        json_string = json.dumps(api_key_list)
+        with open("api_keys.txt", "w") as f:
+            f.write(json_string)
+    else:
+        json_string = json.dumps([api_key])
+        with open("api_keys.txt", "w") as f:
+            f.write(json_string)
     return JSONResponse(content={"status_code": 200, "api_key": api_key})
 
 
 
 @app.post("/regeneratemodel", response_class=HTMLResponse)
-def generate_api_key(request: Request, file: UploadFile = File(...)):
+def regeneratemodel(request: Request, file: UploadFile = File(...), api_key: str=Form(...)):
+    with open("api_keys.txt", "r") as f:
+        json_string = f.read()
+    api_key_list = json.loads(json_string)
+    if api_key not in api_key_list:
+        return JSONResponse(content={"status_code": 401, "message": "Invalid API key"})
     regenerate_model(file.file)
 
 
 @app.post("/oppornot", response_class=HTMLResponse)
-def generate_api_key(request: Request, file: UploadFile = File(...)):
+def regeneratemodel(request: Request, file: UploadFile = File(...), api_key: str=Form(...)):
+    with open("api_keys.txt", "r") as f:
+        json_string = f.read()
+    api_key_list = json.loads(json_string)
+    if api_key not in api_key_list:
+        return JSONResponse(content={"status_code": 401, "message": "Invalid API key"})
     prediction_results = oppornot(file.file)
     new_prediction_results = prediction_results.to_dict(orient="records")
     return JSONResponse(content={"message": "Request processed successfully", "data": new_prediction_results})
